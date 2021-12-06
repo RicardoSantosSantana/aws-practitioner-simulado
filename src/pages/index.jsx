90ea6ca0c79cc4a6f54d17975d6434e291dfd2c0
RicardoSantosSantana <ricardo@needsolution.com.br>
2021-12-06 16:15:44 -0300

    Testes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { ContextSimulacao } from "../context";
 import React, { useContext } from  "react"
 import { Nav, Results } from "../components/Main";
 import  Perguntas  from '../model/perguntas.ts';
+import BGImage from "../components/BGImage.tsx";
 export function getServerSideProps(){
 
     const pergunta = new Perguntas();
@@ -21,10 +22,13 @@ export default function Home(props) {
    
     const { acertos, erros } = useContext(ContextSimulacao); 
     return (        
-            <div className="container">          
+           <div className="container">  
+            
                 <Nav><Results errors={erros} successes={acertos} length = {props.data.length} /></Nav>                                                                 
                 <Simulado data={props.data}></Simulado>
+                 
             </div>        
+        
         )
  
 }
