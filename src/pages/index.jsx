b83e64ced83af10b03822b276b35d3364f30be3d
RicardoSantosSantana <ricardo@needsolution.com.br>
2021-12-08 16:12:14 -0300

    nova fase perguntas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { ContextSimulacao } from "../context";
 import React, { useContext } from  "react"
 import { Nav, Results } from "../components/Main";
 import  Perguntas  from '../model/perguntas.ts';
-import BGImage from "../components/BGImage.tsx";
+
 export function getServerSideProps(){
 
     const pergunta = new Perguntas();
