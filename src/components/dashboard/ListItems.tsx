c908f0a090ed1f1d3ca48bf1cc103dca1de8dd08
RicardoSantosSantana <ricardo@needsolution.com.br>
2021-12-14 18:54:13 -0300

    Mudanca de layout

---
+++ src/components/dashboard/ListItems.tsx
@@ -0,0 +1,32 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard'; 
+import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
+import Link from '@mui/material/Link';
+ 
+export default function ListItems() {
+  return (
+      <div>
+        <ListItem button>
+            <ListItemIcon>
+                <DashboardIcon />
+            </ListItemIcon>
+            <Link   underline="none" href="/" color="inherit">
+                <ListItemText primary="Exercício" />
+            </Link>
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <QuestionAnswerOutlinedIcon />
+            </ListItemIcon>
+            <Link   underline="none" href="/perguntasFrequentes" color="inherit">
+                <ListItemText primary="Perguntas" />
+            </Link>
+        </ListItem>     
+      </div>
+  )
+
+}
+ 
+ 
