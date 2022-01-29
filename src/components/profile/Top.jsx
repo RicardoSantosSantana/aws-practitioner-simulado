491a235998fa8140102f38b18109f4d1ac32ba0b
RicardoSantosSantana <ricardo@needsolution.com.br>
2022-01-29 17:44:09 -0300

    add Image top

--- src/components/profile/Top.jsx
+++ src/components/profile/Top.jsx
@@ -1,21 +1,19 @@
-import Image from 'next/image'
-import absoluteUrl from 'next-absolute-url'
+import Image from 'next/image' 
 import Link from '@mui/material/Link';
-
+ 
 export default function Top(){
-  
-    const { origin } = absoluteUrl()
-       
-    const myLoader = ({ src, width, quality }) => {
-        return `${origin}${src}?w=${width}&q=${quality || 100}`
+        
+    const myLoader = ({ src, width, quality }) => {      
+        return `https://aws-practitioner-simulado.vercel.app./ricardo-santana.png?w=${width}&q=${quality || 100}`      
       }
+      
     return (
     <>
         <Link underline="none" target="__blank" href="https://www.linkedin.com/in/ricardo-santos-santana/" >
         
             <Image
             loader={myLoader} 
-            src="./ricardo-santana.png"
+            src="https://aws-practitioner-simulado.vercel.app./ricardo-santana.png"
             alt="Picture of the author"
             width="350px"
             height="120px"
