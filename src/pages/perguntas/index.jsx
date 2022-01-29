b48a539abbf5dae62d718c3c2afebd614136b4b4
RicardoSantosSantana <ricardo@needsolution.com.br>
2022-01-29 17:32:13 -0300

    add Top image

--- src/pages/perguntas/index.jsx
+++ src/pages/perguntas/index.jsx
@@ -6,7 +6,7 @@ import Container from '@mui/material/Container';
 import { ContextSimulacao } from '../../context/Index';
 import { useContext, useEffect } from "react";
 import axios from 'axios';
- 
+import Top from '../../components/profile/Top';
 IndexPerguntas.getInitialProps = async()=> {          
     const response = await axios.get('https://aws-api-theta.vercel.app/api/faq')    
     return { dados: response.data }
@@ -24,6 +24,7 @@ export default function  IndexPerguntas( { dados } ){
         <Bar theme={ theme }  mode={ mode } setMode = { setMode } titleRight={<LikeDislike   show="false" theme={ theme }/> } titleLeft="Perguntas">
              
             <Container>
+            <Top></Top>
                <Header showResumo="false" theme={ theme }></Header> 
                 <Content dados={dados} key="content"  theme={ theme } />
              </Container>      
